6e9865affcd7d667ff8bd432ae759563a3f8b301
Asim <kzasim513@gmail.com>
2025-04-19 00:25:17 +0500

    Update footer

--- src/components/FooterOne.js
+++ src/components/FooterOne.js
@@ -1,14 +1,9 @@
 import Link from 'next/link';
 import React from 'react';
 import {
-  FaArrowRight,
-  FaCalendarAlt,
   FaEnvelope,
-  FaFacebookF,
-  FaInstagram,
   FaPhoneAlt,
-  FaTwitter,
-  FaYoutube,
+
 } from 'react-icons/fa';
 
 const FooterOne = () => {
@@ -41,160 +36,34 @@ const FooterOne = () => {
             <div className="col-lg-6 col-md-6">
               <div className="widget widget_about">
                 <div className="thumb">
-                  <img src="assets/img/logo2.png" alt="img" />
+                <Link href="/index-1">
+             <h3 className='text-white '>Telecompaybill.us </h3>
+            </Link>
                 </div>
                 <div className="details">
                   <p>
                     Leading provider of 5G and fiber-optic <br /> internet solutions for homes and businesses
                   </p>
+                 
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-6 col-md-6">
+              <div className="widget widget_about">
+                
+                <div className="details">
                   <p className="mt-3">
                     <FaPhoneAlt />  +1 888 427 7915
                   </p>
                   <p className="mt-2">
                     <FaEnvelope /> support@ Telecompaybill.us.com
                   </p>
-                  {/* <ul className="social-media">
-                    <li>
-                      <Link href="#">
-                        <FaFacebookF />
-                      </Link>
-                    </li>
-                    <li>
-                      <Link href="#">
-                        <FaTwitter />
-                      </Link>
-                    </li>
-                    <li>
-                      <Link href="#">
-                        <FaInstagram />
-                      </Link>
-                    </li>
-                    <li>
-                      <Link href="#">
-                        <FaYoutube />
-                      </Link>
-                    </li>
-                  </ul> */}
+                 
                 </div>
               </div>
             </div>
-            <div className="col-lg-3 col-md-6">
-              <div className="widget widget_nav_menu">
-                <h4 className="widget-title">Our Services</h4>
-                <ul>
-                  <li>
-                    <Link href="/services">
-                      <FaArrowRight /> 5G Mobile Plans
-                    </Link>
-                  </li>
-                  <li>
-                    <Link href="/services">
-                      <FaArrowRight /> Fiber Internet
-                    </Link>
-                  </li>
-                  <li>
-                    <Link href="/services">
-                      <FaArrowRight /> Business Solutions
-                    </Link>
-                  </li>
-                  <li>
-                    <Link href="/services">
-                      <FaArrowRight /> Home WiFi Setup
-                    </Link>
-                  </li>
-                  <li>
-                    <Link href="/services">
-                      <FaArrowRight /> TV & Streaming
-                    </Link>
-                  </li>
-                  <li>
-                    <Link href="/services">
-                      <FaArrowRight /> 24/7 Support
-                    </Link>
-                  </li>
-                </ul>
-              </div>
-            </div>
-            <div className="col-lg-3 col-md-6">
-              <div className="widget widget_nav_menu">
-                <h4 className="widget-title">Quick Links</h4>
-                <ul>
-                  <li>
-                    <Link href="/about">
-                      <FaArrowRight /> About Provider
-                    </Link>
-                  </li>
-                  <li>
-                    <Link href="#">
-                      <FaArrowRight /> Coverage Map
-                    </Link>
-                  </li>
-                  <li>
-                    <Link href="#">
-                      <FaArrowRight /> Support Center
-                    </Link>
-                  </li>
-                  <li>
-                    <Link href="#">
-                      <FaArrowRight /> FAQ
-                    </Link>
-                  </li>
-                  <li>
-                    <Link href="#">
-                      <FaArrowRight /> Careers
-                    </Link>
-                  </li>
-                  <li>
-                    <Link href="/contact">
-                      <FaArrowRight /> Contact Us
-                    </Link>
-                  </li>
-                </ul>
-              </div>
-            </div>
-            {/* <div className="col-lg-3 col-md-6">
-              <div className="widget widget-recent-post">
-                <h4 className="widget-title">Latest News</h4>
-                <ul>
-                  <li>
-                    <div className="media">
-                      <div className="media-left">
-                        <img src="assets/img/widget/1.png" alt="blog" />
-                      </div>
-                      <div className="media-body align-self-center">
-                        <div className="post-info mb-2">
-                          <FaCalendarAlt />
-                          <span>January 11, 2023</span>
-                        </div>
-                        <h6 className="title mb-0">
-                          <Link href="/blog-details">
-                            5G Network Expansion Update
-                          </Link>
-                        </h6>
-                      </div>
-                    </div>
-                  </li>
-                  <li>
-                    <div className="media">
-                      <div className="media-left">
-                        <img src="assets/img/widget/2.png" alt="blog" />
-                      </div>
-                      <div className="media-body align-self-center">
-                        <div className="post-info mb-2">
-                          <FaCalendarAlt />
-                          <span>January 5, 2023</span>
-                        </div>
-                        <h6 className="title mb-0">
-                          <Link href="/blog-details">
-                            New Fiber Optic Packages
-                          </Link>
-                        </h6>
-                      </div>
-                    </div>
-                  </li>
-                </ul>
-              </div>
-            </div> */}
+           
+            
           </div>
         </div>
         <div className="footer-bottom">
